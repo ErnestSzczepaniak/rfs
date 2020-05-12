89d40112ec7d6275204ea940772a8ba6ab29e315
en2 <ernest.szczepaniak.en2@gmail.com>
2020-05-12 19:39:26 +0200

    cmake

--- cmake/CMakeConfigBasic.cmake
+++ cmake/CMakeConfigBasic.cmake
@@ -32,15 +32,18 @@ set(dependency_scope            PRIVATE)
 
 set(architecture                v7a)
 set(core                        a9)
-set(gic				            pl390)
+set(gic				pl390)
 set(family                      c5)
 
 set(board                       de10standard)
 
 set(sdram_start                 0x00000000)
 set(sdram_size                  1024M)
+set(sdram_size_stack            2M)
+set(sdram_size_pool             512M)
+
 set(ocram_start                 0xffff0000)
 set(ocram_size                  64K)
-set(stack_size                  128K)
-set(pool_size                   900M)
+set(ocram_size_stack            4K)
+set(ocram_size_pool             48K)
 
